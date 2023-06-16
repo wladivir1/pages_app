d094a66c3dbd3eb1d74168b9a8ba97fe21802775
vv <test@com>
2023-06-16 22:54:38 +0300

    initial commit

---
+++ pages/tests.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+
+
+class SimpleTests(SimpleTestCase):
+    def test_home_psge_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEquals(response.status_code, 200)
+
+# Create your tests here.
